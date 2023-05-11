62b62c84c643bdcf31cbee8a8e6491b2fd29285a
Jerry Nkongolo <jerrynkongolo@icloud.com>
2023-05-11 23:59:20 +0300

    Initial Commit

---
+++ SearchBarFilter/SearchBarFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarFilterApp.swift
+//  SearchBarFilter
+//
+//  Created by jerry nkongolo on 2023-05-11.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
